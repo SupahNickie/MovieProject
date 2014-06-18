39772a604e11d37502af17a1a668b767b65d0a11
SupahNickie <nicholascase@live.com>
2014-06-17 21:29:04 -0700

    Logic for looping getting there, including poster graphics

--- js/script.js
+++ js/script.js
@@ -5,23 +5,29 @@ var searchTomatoes = document.getElementById("tomatoes_boolean")
 
 $(document).ready(function () {
   $('#submit_button').click(function() {
-    $('#results_fill').html("");
+    $('#results_fill').html(""); // Clear the results without refreshing page
     $.getJSON( "http://www.omdbapi.com/?s=" + searchString.value + "&t=" + searchTitle.value + "&y=" + searchYear.value + "&tomatoes=" + searchTomatoes, function(data) {
       if (searchString.value.length > 0) {
         $('#header').html("<h1>" + data.Search[0].Title + "</h1>");
-        $.each(data.Search[0], function(index, value) {
-          if (value == "N/A" || index == "Title") {
-            // Skip
-          } else {
-          $('#results_fill').append("<p>" + index + ": " + value + "</p>");
-          }
+        $.each(data.Search, function(index, value) {
+          $.each(data.Search[index], function(index, value) {
+            if (value == "N/A" || index == "Response") {
+              // Skip useless information
+            } else if (index == "Poster") {
+              $('#results_fill').append("<img src=" + value + "</img>");
+            } else {
+              $('#results_fill').append("<p>" + index + ": " + value + "</p>");
+            }
+          })
         });
       } else {
         $('#header').html("<h1>" + data.Title + "</h1>");
         $.each(data, function(index, value) {
-          if (value == "N/A" || index == "Title") {
-            // Skip
-          } else {
+          if (value == "N/A" || index == "Title" || index == "Response" || index == "Type") {
+            // Skip useless information
+          } else if (index == "Poster") {
+            $('#results_fill').append("<img src=" + value + "</img>");
+          } else{
             $('#results_fill').append("<p>" + index + ": " + value + "</p>");
           }
         });
